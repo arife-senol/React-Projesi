51881421d176b7801d4fa5dc9849fdaf08611abb
arife-senol <avcisenolarife@gmail.com>
2024-06-14 16:28:23 +0300

    App module css file and package json added

--- react-witflix/src/App.jsx
+++ react-witflix/src/App.jsx
@@ -1,44 +1,38 @@
-import './App.css'
-import { Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Welcome from './pages/Welcome';
-import Home from './pages/Home';
-import Header from './components/Header/Header';
-import { Link, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
+import { Link, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
 
 function App() {
-
+  const [activeProfile, setActiveProfile] = useState(null);
   return (
     <>
- 
-     <Switch>
-     <Route exact path="/">   
-     <h1>Welcome to Netflix by Wit</h1>
-     </Route>
-     <Route exact path="/login">
-     <Header/>
-       <Login/>
-     </Route>
-     <Route exact path="/welcome">
-       <Welcome/>
-     </Route>
-     <Route path="/home">
-     <Header/>
-       <Home/>
-     </Route>
-     </Switch>
-<div className= "dev-navigation">
-<Link to="/">HomePage</Link>
-<Link to="/login">Login</Link>
-<Link to="/welcome">Welcome</Link>
-<Link to="/home">Home</Link>
-
-
-</div>
-
-
-
-
+      <Switch>
+        <Route exact path="/">
+          <h1>Welcome to Netflix by Wit</h1>
+        </Route>
+        <Route exact path="/login">
+          <Header />
+          <Login />
+        </Route>
+        <Route exact path="/welcome">
+          <Welcome setActiveProfile={setActiveProfile} />
+        </Route>
+        <Route path="/home">
+          <Header />
+          <Home activeProfile = {activeProfile}/>
+        </Route>
+      </Switch>
+      <div className="dev-navigation">
+        <Link to="/">HomePage</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/home">Home</Link>
+      </div>
     </>
   );
 }
