63973d6272063e367725ad8e2722d38914d8471b
arife-senol <avcisenolarife@gmail.com>
2024-06-14 16:23:30 +0300

    Home and Welcome page configured

--- react-witflix/src/pages/Home.jsx
+++ react-witflix/src/pages/Home.jsx
@@ -1,4 +1,33 @@
-export default function Home() {
-  return <div>Home</div>;
+import { useEffect, useState } from "react";
+import { userSuggestionsData } from "../userSuggestions";
+import Suggestion from "../components/Suggestion/Suggestion";
+import Hero from "../components/Hero/Hero";
 
+/* eslint-disable react/prop-types */
+export default function Home(props) {
+  const [suggestions, setSuggestions] = useState([]);
+  const { activeProfile } = props;
+  const { name } = activeProfile;
+
+  useEffect(() => {
+    const suggestionData = userSuggestionsData[activeProfile.id];
+    setSuggestions(suggestionData);
+  }, [activeProfile]);
+
+  useEffect(() => {
+    console.log("mount oldu"+name);
+    return function () {
+      //return'de fonksiyon unmount eventi
+      console.log("unmount oldu");
+    };
+  }, []); //boş dependency array mount eventi
+
+  return (
+    <>
+      <Hero activeProfile={activeProfile} />
+      {suggestions.map((suggestion, index) => {
+        return <Suggestion key={index} suggestion={suggestion} />;
+      })}
+    </>
+  );
 }
--- react-witflix/src/pages/Welcome.jsx
+++ react-witflix/src/pages/Welcome.jsx
@@ -1,3 +1,33 @@
-export default function Welcome() {
-  return  <div>Welcome</div>;
+/* eslint-disable react/prop-types */
+
+import Profiles from "../components/Profile/Profiles";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.div`
+  color: gray;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid gray;
+  display: inline-block;
+`;
+
+export default function Welcome(props) {
+  const { setActiveProfile } = props;
+
+  return (
+    <Container>
+      <h1>Who is watching?</h1>
+      <Profiles setActiveProfile={setActiveProfile} />
+      <br />
+      <Button>Manage Profiles</Button>
+    </Container>
+  );
 }
