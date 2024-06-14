7d661ca31217f20f9754e13a3bdfa7e5a94c7394
arife-senol <avcisenolarife@gmail.com>
2024-06-14 16:26:43 +0300

    suggestion component and profiles dummy data added

---
+++ react-witflix/src/components/Suggestion/Suggestion.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import styled from "styled-components";
+import Movies from "./Movies";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Card = styled.div`
+  padding: 1rem;
+`;
+
+const Title = styled.h2`
+  color: white;
+  text-align: left;
+`;
+
+export default function Suggestion(props) {
+  const [movies, setMovies] = useState([]);
+  const { suggestion } = props;
+  const { title, category } = suggestion;
+
+  useEffect(() => {
+    /*    
+    Farklı şekilde http isteği
+    const options = {
+      method: "GET",
+      url: "https://movies-api14.p.rapidapi.com/movies",
+      headers: {
+        "x-rapidapi-key": "2ce04038e2msh104054e193ec289p18cdf9jsnb6a4a44d19e9",
+        "x-rapidapi-host": "movies-api14.p.rapidapi.com",
+      },
+    }; */
+
+    axios
+      .get("https://movies-api14.p.rapidapi.com/" + category, {
+        headers: {
+          "x-rapidapi-key":
+            "2ce04038e2msh104054e193ec289p18cdf9jsnb6a4a44d19e9",
+          "x-rapidapi-host": "movies-api14.p.rapidapi.com",
+        },
+      })
+      .then((response) => {
+        //console.log(response.data);
+        const random = Math.floor(
+          Math.random() * (response.data.movies.length - 6)
+        );
+        setMovies(response.data.movies.slice(random, random + 6)); //response.data)
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }, [category]);
+
+  return (
+    <Card>
+      <Title>{title}</Title>
+      <Movies movies={movies} />
+    </Card>
+  );
+}
---
+++ react-witflix/src/userSuggestions.js
@@ -0,0 +1,22 @@
+export const userSuggestionsData = {
+    7: [
+      {
+        title: "Today's pick for you",
+        category: "movies",
+      },
+      {
+        title: "Continue Watching for Arife",
+        category: "shows",
+      },
+    ],
+    8: [
+      {
+        title: "Today's pick for you",
+        category: "movies",
+      },
+      {
+        title: "Continue Watching for Kids",
+        category: "shows",
+      },
+    ],
+  };
